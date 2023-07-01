c83dd14130d30e222d57a5d8c36bf6855437b173
Bhushan Thombre <80145921+Bhushan-Thombre@users.noreply.github.com>
2023-07-01 21:20:47 +0530

    Time: 754 ms (5.21%), Space: 39.8 MB (69.27%) - LeetHub

---
+++ 1418-fair-distribution-of-cookies/1418-fair-distribution-of-cookies.java
@@ -0,0 +1,40 @@
+class Solution {
+
+
+    // zeroCount represent number of children with no cookies.
+    // distribute array represent the number of cookies each child contains after fair distribution.
+
+
+    public int dfs(int i, int[] distribute, int[] cookies, int k, int zeroCount) {
+        if (cookies.length - i < zeroCount) {
+            return Integer.MAX_VALUE;
+        }
+
+        if (i == cookies.length) {
+            int unfairness = Integer.MIN_VALUE;
+            for (int value : distribute) {
+                unfairness = Math.max(unfairness, value);
+            }
+            return unfairness;
+        }
+
+        int answer = Integer.MAX_VALUE;
+        for (int j = 0; j < k; j++) {
+            zeroCount -= distribute[j] == 0 ? 1 : 0;
+            distribute[j] += cookies[i];
+
+            answer = Math.min(answer, dfs(i + 1, distribute, cookies, k, zeroCount));
+
+            zeroCount += distribute[j] == 0 ? 1: 0;
+            distribute[j] -= cookies[i];
+
+        }
+
+        return answer;
+    }
+    public int distributeCookies(int[] cookies, int k) {
+        int[] distribute = new int[k];
+
+        return dfs(0, distribute, cookies, k, k);
+    }
+}
